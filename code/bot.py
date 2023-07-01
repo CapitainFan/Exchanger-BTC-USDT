f38f42ae42e33d201b36563c73b012d2cc27a986
Sokolnikov Bogdan <sokolnikovbogdan1@gmail.com>
2023-07-01 15:43:28 +0300

    some fixes

--- code/bot.py
+++ code/bot.py
@@ -7,11 +7,9 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 load_dotenv()
 
 TOKEN = os.getenv('TOKEN')
-TEXT = '''Здравстуй дорогой пользователь!
-Это телеграмм бот, который получает текущий курс BTC/USDT;
-Позволяет продовать или покупать валютную пару 
-BTC-WXG/USDT-WXG на криптоирже waves.exchange.
-!!Бот работает только seed аккаунтами (без email)!!
+TEXT = '''Бот запущен!
+Для покупки или продажи BTC/USDT
+войдите в ваш аккаунт waves.exchange
 '''
 
 bot = Bot(token=TOKEN)
